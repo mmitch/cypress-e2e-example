ce789c040cfb8e291033c04fc74f8bd874ccce0a
Christian Garbs <mitch@cgarbs.de>
2022-09-05 16:55:10 +0200

    add combined mochawesome HTML report

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
     reporter: 'cypress-multi-reporters',
     reporterOptions: {
-	configFile: 'reporter-config.json'
+	configFile: 'reporter-config.json',
     },
     e2e: {
 	baseUrl: 'https://www.cgarbs.de',
